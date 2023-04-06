4158e3819c6b7a78d0fab8dc119295cde91057d0
Kenjidoom <vitodevcool@protonmail.com>
2023-04-05 18:13:37 -0700

    Exception added for exiting program

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ import os
 def alarm():
     # Murica
     print("Enter the hour you want the glock to go off at: ") 
+    #time.sleep(1 * 3600)
     time.sleep(1)
     notification()
 
 def notification():
     print("WAKE UP WAKE UP WAKE UP")
-    sound = AudioSegment.from_wav('/home/kenji/Desktop/WAKEUP-APP/sound-affects/alarm-affect.wav')
-    play(sound)
-
+    try:
+        while True:
+            sound = AudioSegment.from_wav('/home/kenji/Desktop/WAKEUP-APP/sound-affects/alarm-affect.wav')
+            play(sound)
+    except KeyboardInterrupt:
+        print("exiting...")
 alarm()
 
