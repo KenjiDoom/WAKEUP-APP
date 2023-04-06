ca22dbdb0b83c27fa2e6885b3a2ec70e6d6b5bd2
Kenjidoom <vitodevcool@protonmail.com>
2023-04-05 18:06:55 -0700

    changed the streamer library
    Also, I will make the location of the file more dynamic once I get the chance.

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from playsound import playsound
+from pydub import AudioSegment
+from pydub.playback import play
 import time
 import os
 
-
 def alarm():
+    # Murica
     print("Enter the hour you want the glock to go off at: ") 
     time.sleep(1)
     notification()
 
 def notification():
     print("WAKE UP WAKE UP WAKE UP")
-    playsound('ALARM_SOUND_AFFECT.mkv')
+    sound = AudioSegment.from_wav('/home/kenji/Desktop/WAKEUP-APP/sound-affects/alarm-affect.wav')
+    play(sound)
 
 alarm()
 
