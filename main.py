3604ebe09ef3e2efa94f9a975135c98de9b94c36
Kenjidoom <vitodevcool@protonmail.com>
2023-04-08 10:17:36 -0700

    changed sound sound library to pygame

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from pydub import AudioSegment
-from pydub.playback import play
 from PIL import ImageTk, Image
 from tkinter import messagebox
+import time, sys, os, pygame
 from tkinter import *
-import time, sys, os
+ 
+pygame.mixer.init()
 
 def start_alarm():
     try:
@@ -22,21 +22,15 @@ def start_alarm():
         time.sleep(1)
 
         if (total_time == 0):
-            print("POP MESSAGE")
+            pygame.mixer.music.load("/home/kenji/Desktop/WAKEUP-APP/affects/alarm-affect.wav")
+            pygame.mixer.music.play(loops=0)
             messagebox.showinfo("WAKE UP", "WAKE UP TIME TO WAKE UP!!!!")
+            exit()
 
         total_time -= 1
 
-def stop_alarm():
+def stop_program():
     exit()
-
-def alarm():
-    try:
-        while True:
-            sound = AudioSegment.from_wav('/home/kenji/Desktop/WAKEUP-APP/affects/alarm-affect.wav')
-            play(sound)
-    except KeyboardInterrupt:
-        print("Exiting....")
 
 window = Tk()
 window.title("WAKE UP!")
@@ -45,24 +39,29 @@ minute=StringVar()
 second=StringVar()
 
 minute.set("00")
-second.set("03")
+second.set("01")
 
-fpack = ("Arial",24)
+fpack = ("Noto Sans Mandaic",40)
 
 canvas1 = Canvas(window, width=650, height=500, relief='raised')
 canvas1.pack()
 
+
+# Minute Label
 minutes_box = Label(window, width=3, font=fpack, textvariable=minute)
 minutes_box.place(x=170, y=100)
 
+# Second Label
 seconds_box = Label(window, width=3, font=fpack, textvariable=second)
-seconds_box.place(x=250, y=100)
+seconds_box.place(x=300, y=100)
 
+# Start button
 start_button = Button(text="Start", command=start_alarm)
                         #width? #height
 canvas1.create_window(250, 400, height=50, width=150, window=start_button)
-   
-stop_button = Button(text="Stop", command=stop_alarm)
+
+# Stop Button   
+stop_button = Button(text="Stop", command=stop_program)
 canvas1.create_window(400, 400, height=50, width=150, window=stop_button)
 
 window.resizable(False, False)
