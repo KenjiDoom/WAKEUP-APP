b380aa31a88c49c9dad13ba9f39f46132a8758dc
kenjidoom <77996865+KenjiDoom@users.noreply.github.com>
2023-04-11 20:18:15 -0700

    Im stuck....
    I cant figure out why the program wont stop whilst the count down timer is running

--- main.py
+++ main.py
@@ -2,11 +2,24 @@ from PIL import ImageTk, Image
 from tkinter import messagebox
 from CTkMessagebox import CTkMessagebox
 import time, sys, os, pygame, customtkinter
+from threading import *
 from tkinter import *
  
 customtkinter.set_appearance_mode("light")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("green")  # Themes: blue (default), dark-blue, green
 pygame.mixer.init()
+
+
+def threading(button_press):
+    if button_press == 'start':
+        t1 = Thread(target=start_alarm())
+        t1.start()
+    elif button_press == 'stop':
+        t2 = Thread(target=stop_program())
+        t2.start()
+    elif button_press == 'play_sound':
+        t3 = Thread(target=play_sound())
+        t3.start()
 
 def start_alarm():
     try:
@@ -25,29 +38,32 @@ def start_alarm():
         time.sleep(1)
 
         if (total_time == 0):
-            pygame.mixer.music.load("/home/kenji/Desktop/WAKEUP-APP/affects/alarm-affect.wav")
-            pygame.mixer.music.play(loops=0)
-            #messagebox.showinfo("WAKE UP", "WAKE UP TIME TO WAKE UP!!!!")
-            msg = CTkMessagebox(title="WAKE UP!", message="1 HOUR OF CODE IS UP!",
-                  icon="warning", option_1="Exit")
-    
-            if msg.get()=="Exit":
-                exit()
-                
+            threading('play_sound')
+
         total_time -= 1
+
+def play_sound():
+    pygame.mixer.music.load('/home/kenji/Desktop/WAKEUP-APP/affects/alarm-affect.wav')
+    pygame.mixer.music.play(loops=0)
+    result = messagebox.askyesno('Timer is up!', "The program is done counting, would you like to exit?", icon='warning')
+
+    if result == True:
+        exit()
+    elif result == False:
+        pass
 
 def stop_program():
     exit()
 
 window = customtkinter.CTk()
-window.title("WAKE UP!")
+window.title("Coding Timer")
 
 minute=StringVar()
 second=StringVar()
 
 # Fixed Time
 minute.set("00")
-second.set("01")
+second.set("04")
 
 fpack = ("Noto Sans Mandaic bold", 80)
 
@@ -68,21 +84,23 @@ seconds_box = customtkinter.CTkLabel(master=window, width=0, height=0, font=fpac
 seconds_box.place(x=400, y=150, anchor="center")
 
 # Image for Start button
-button_photo = PhotoImage(file='/home/kenji/Desktop/WAKEUP-APP/play-icon.png')
+button_photo = PhotoImage(file='/home/kenji/Desktop/WAKEUP-APP/affects/play-icon.png')
 photo_image_one = button_photo.subsample(10, 10)
 # Start button
-start_button = Button(master=window, text="Start", background='white', image=photo_image_one, command=start_alarm)
+start_button = Button(master=window, text="Start", background='white', image=photo_image_one, command=lambda m='start': threading(m))
                         #width? #height
 canvas1.create_window(250, 400, height=50, width=150, window=start_button)
 
 # Image for Stop button
-stop_photo = PhotoImage(file='/home/kenji/Desktop/WAKEUP-APP/stop-icon.png')
+stop_photo = PhotoImage(file='/home/kenji/Desktop/WAKEUP-APP/affects/stop-icon.png')
 photo_image_two = stop_photo.subsample(17, 17)
 
 # Stop Button   
-stop_button = Button(master=window, text="Stop", background='white', image=photo_image_two, command=stop_program)
+stop_button = Button(master=window, text="Stop", background='white', image=photo_image_two, command=lambda m='stop': threading(m))
 canvas1.create_window(410, 400, height=50, width=150, window=stop_button)
 
-window.resizable(False, False)
-window.mainloop()
+
+if __name__ == "__main__":
+    window.resizable(False, False)
+    window.mainloop()
 
